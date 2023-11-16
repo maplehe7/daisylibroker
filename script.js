a359e627a9dab4e83c013f4e79a906d0578ab166
MapleHe <13944805-MapleHe@users.noreply.replit.com>
2023-11-16 00:05:28 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function myFunction(x) {
+  x.classList.toggle("change");
+  
+}
+console.log('%cThis website was made by Ethan He. You can support him by donating to his gofundme. He has permanent leg and brain damage and will have died by the time you are reading this.', 'color: green; font-size: 20px; ');
